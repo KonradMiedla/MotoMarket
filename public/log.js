473f5e1af72239ba5c682f1fe679874a8c17bb51
KonradMiedla <konrad.miedla@gmail.com>
2022-06-21 17:27:28 +0200

    first

---
+++ public/log.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#registerDiv').removeClass('active');
+		$('#loginDiv').addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#loginDiv').removeClass('active');
+		$('#registerDiv').addClass('active');
+		e.preventDefault();
+	});
+});
